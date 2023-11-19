9eba7cb341d18da404a951797726ceda5ce53084
Tyuiu.KropchevSR@outlook.com <Tyuiu.KropchevSR@outlook.com>
2023-11-19 17:33:22 +0500

    Add project files.

---
+++ Tyuiu.KropchevSR.Sprint5.Task0.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KropchevSR.Sprint5.Task0.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KropchevSR.Sprint5.Task0.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9de9a89c-fb0b-4d9d-a695-6d063927844c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KropchevSR.Sprint5.Task0.V8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KropchevSR.Sprint5.Task0.V8.Lib;
+
+namespace Tyuiu.KropchevSR.Sprint5.Task0.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Кропчев СР | АСОиУБ-23-1";
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                            *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                                     *");
+            Console.WriteLine("* Таск #0                                                                              *");
+            Console.WriteLine("* Вариaнт #8                                                                           *");
+            Console.WriteLine("* Выполнил: Кропчев Степан Романович | АСОиУБ-23-1                                     *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                             *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в               *");
+            Console.WriteLine("* файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх знаков после запятой*");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
+            Console.WriteLine("****************************************************************************************");
+            int x = 4;
+            Console.WriteLine(x);
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                           *");
+            Console.WriteLine("****************************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл" + res);
+            Console.ReadKey();
+        }
+    }
+}
+
