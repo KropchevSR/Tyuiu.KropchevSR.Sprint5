f8e02167a4741b07582683773f264f59214ffb90
Tyuiu.KropchevSR@outlook.com <Tyuiu.KropchevSR@outlook.com>
2023-11-26 19:19:33 +0500

    Task6

---
+++ Tyuiu.KropchevSR.Sprint5.Task6.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KropchevSR.Sprint5.Task6.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KropchevSR.Sprint5.Task6.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4f5bb62-14f7-4a40-97bd-0ca07c0fc196")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KropchevSR.Sprint5.Task6.V13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KropchevSR.Sprint5.Task6.V13.Lib;
+
+namespace Tyuiu.KropchevSR.Sprint5.Task6.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Кропчев СР | АСОиУБ-23-1";
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                            *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                                     *");
+            Console.WriteLine("* Таск #6                                                                              *");
+            Console.WriteLine("* Вариaнт #13                                                                          *");
+            Console.WriteLine("* Выполнил: Кропчев Степан Романович     | АСОиУБ-23-1                                 *");
+            Console.WriteLine("****************************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V13.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("*****************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТAТ:                                                                            *");
+            Console.WriteLine("*****************************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
